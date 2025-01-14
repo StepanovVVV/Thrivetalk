97dcbc9f697f95475c0f6e59cbbb130f63bdb35b
StepanovVVV <98964014+StepanovVVV@users.noreply.github.com>
2025-01-14 18:06:24 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,6 +81,31 @@ $(document).ready(function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function (lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        lazyImages.forEach(function (lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+        });
+    }
+});
+
 //Remove placeholder on click
 $('input,textarea').on('focus', function () {
     var $this = $(this);
